bfc22dd2f0d0e860a71601b646e80ece48e2d20f
TheMarkitecht <Mark@TheMarkitecht.com>
2020-02-07 09:26:11 -0600

    Test for finalizer.

---
+++ test/70-finalize.tcl
@@ -0,0 +1,39 @@
+# slim
+# Copyright 2020 Mark Hubbard, a.k.a. "TheMarkitecht"
+# http://www.TheMarkitecht.com
+#
+# Project home:  http://github.com/TheMarkitecht/slim
+# slim is an object-oriented programming package for Jim Tcl (http://jim.tcl.tk/)
+# slim helps you develop well-organized object-oriented apps in Tcl.
+#
+# This file is part of slim.
+#
+# slim is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# slim is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with slim.  If not, see <https://www.gnu.org/licenses/>.
+
+proc test {} {
+    setup Pet
+
+    Pet method destroy {} {
+        set ::done 1
+    }
+    set ::done 0
+    set p [Pet new]
+    collect
+    assert {$::done == 0}
+    collect
+    assert {$::done == 0}
+    set p {}
+    collect
+    assert {$::done == 1}
+}
